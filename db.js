bde7e7328d111d077cc4b6a8c29ac42d7afe669f
firhanabdurr <firhanabdurr@gmail.com>
2022-11-01 00:08:08 +0700

    add

---
+++ db.js
@@ -0,0 +1,28 @@
+var mysql = require('mysql');
+
+module.exports = {
+    getData: function (sql, param, callback) {
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'pharmacy_database'
+        });
+
+        connection.connect(function (err) {
+            if (err) {
+                console.log('error connecting database ...');
+            }
+        });
+        if (param == null) {
+            connection.query(sql, function (err, result) {
+                callback(result);
+            });
+        }
+        else {
+            connection.query(sql, param, function (err, result) {
+                callback(result);
+            });
+        }
+    }
+};
